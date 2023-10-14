460e2371c242dc6a97e395fb3aefd1fedbd7a807
Artyom <hubabuba0144@gmail.com>
2023-10-14 20:33:08 +0300

    base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int max = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+System.Console.Write("max = ");
+Console.WriteLine(max);
+
