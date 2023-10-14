6dc5ab5080a70b382419737434fd5832b3a35108
Artyom <hubabuba0144@gmail.com>
2023-10-14 20:24:53 +0300

    added Example005_Greeting with condition

---
+++ Example005_Greeting/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    System.Console.WriteLine("Привет, незнакомец");
+}
