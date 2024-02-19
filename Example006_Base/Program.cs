e7809c2c73608ae09af6b3a3ec583a3b6c370c9c
Artyom <hubabuba0144@gmail.com>
2024-02-19 19:03:28 +0300

    added @if@ base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 1;
+int b = 5;
+int c = 7;
+int d = 3;
+int e = 9;
+
+int max =a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+
+System.Console.Write("max = ");
+System.Console.WriteLine(max);
