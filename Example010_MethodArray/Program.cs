6e2819f5f03e1a1c3bbee74311f334a995fd4dbf
Artyom <hubabuba0144@gmail.com>
2024-02-22 00:25:52 +0300

    Ex010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = {1,2,3,4,5,56,67,78,8,56};
+
+int n = array.Length;
+int find = 56;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
